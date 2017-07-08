f78629f36e04a5db2b03ee4048be507fdb65b8c8
Donnie Dorsey <ddorsey84@gmail.com>
2017-07-08 15:26:23 -0400

    Pluralsight Demo

--- RaysHotDogs/MainActivity.cs
+++ RaysHotDogs/MainActivity.cs
@@ -7,12 +7,26 @@ namespace RaysHotDogs
     [Activity(Label = "RaysHotDogs", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
+        int count = 1;
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
             // Set our view from the "main" layout resource
             SetContentView (Resource.Layout.Main);
+
+            Button button = FindViewById<Button>(Resource.Id.MyButton);
+
+            
+            button.Click += delegate
+            {
+                button.Text = string.Format("{0} clicks!", count);
+            };
+        }
+
+        private void delegat(object sender, System.EventArgs e)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
